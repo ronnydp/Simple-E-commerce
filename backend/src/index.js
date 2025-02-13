2070c58971439166e88d8b197fe2efcaaa0652e1
ronnydp <ronnydp742@gmail.com>
2025-02-13 01:40:08 -0500

    some routes added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,25 @@
 import express from "express";
-import morgan from "morgan"
+import morgan from "morgan";
+import { Router } from "express";
 
 const app = express();
-
+const router = Router();
 
 app.use(morgan('dev'));
+
+router.get("/", (req, res) => {
+  res.send('e-commerce')
+})
+
+router.get('/products', (req, res) => {
+  res.send('products')
+})
+
+router.get('/customers', (req, res) => {
+  res.send('customers')
+})
+
+app.use('/', router)
 
 app.listen(5000, () => {
   console.log('Server on port 5000');
